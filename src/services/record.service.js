0156917b6ebe0ef75dce7287a65993a19065005d
Horacio Romero <hromero@artedigital-mx.com>
2021-05-29 15:35:58 -0700

    Support update, remove a Record

--- src/services/record.service.js
+++ src/services/record.service.js
@@ -7,3 +7,21 @@ export const getAll = async criteria =>
 
 
 export const getById = async id => await db.Record.findByPk(id);
+
+export const update = async (id, { service_id, user_id, cost }) => 
+    await db.Record.update({ 
+        service_id,
+        user_id,
+        cost
+    }, {
+        where: {
+            id
+        }
+    });
+
+export const remove = async id => 
+    await db.Record.destroy({
+        where: {
+            id
+        }
+    });
