8df287bde9d5e1be475e364a3e5995d67626975b
Horacio Romero <hromero@artedigital-mx.com>
2021-05-28 23:26:19 -0700

    Flat associate objects, finally !!!

--- src/controllers/record.controller.js
+++ src/controllers/record.controller.js
@@ -1,24 +1,32 @@
 import * as recordService from '../services/record.service';
-
+import db from '../models';
 
 export const getAll = async (req, res) => {
     try {                
         let { sort, offset, limit } = req.query;
         limit = limit ?? 10;
-        const perPage = offset ? limit * offset:  0;        
-        const records = await recordService.getAll({ 
-                    order: [ 
-                        [sort ? sort.split(' ') : ['type', 'asc']]
-                    ],
-                    limit,
-                    offset: perPage,
-                    // where: { 
-                    //     username: {
-                    //         [Op.startsWith]: `%@%`
-                    //     }
-                    // }
-                });
+        const perPage = offset ? limit * offset:  0;             
         
+        const criteria = {                      
+            raw: true,
+            order: [ 
+                [sort ? sort.split(' ') : ['service_id', 'asc']]
+            ],
+            limit,
+            offset: perPage,
+            attributes: ['id', 'uuid', 'cost', 'user_balance', 'service_response', 'Service.type', 'User.username'],
+            include: [
+                {
+                    model: db.User,
+                    require: true,
+                    attributes: ['username']
+                },{ 
+                    model: db.Service,
+                    require: true,
+                    attributes: ['type']
+                }] 
+        }
+        const records = await recordService.getAll(criteria);       
         res.status(200).json(records);
 
     } catch(err) {        
